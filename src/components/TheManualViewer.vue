fb97be841cd47868cefb8b2beeac5072e95543df
Ryan Grunest <ryangrunest@geeko>
2021-10-25 11:57:39 -0700

    update button text

--- src/components/TheManualViewer.vue
+++ src/components/TheManualViewer.vue
@@ -28,7 +28,7 @@
           disabled:opacity-50 disabled:cursor-not-allowed
         "
       >
-        Previous Image
+        Previous Page
       </button>
       <button
         @click="nextImageButtonClicked"
@@ -44,7 +44,7 @@
           disabled:opacity-50 disabled:cursor-not-allowed
         "
       >
-        Next Image
+        Next Page
       </button>
     </div>
   </div>
