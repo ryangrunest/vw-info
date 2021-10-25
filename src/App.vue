417bb6cdbfd802422f5887a78aff3def686c58ab
Ryan Grunest <ryangrunest@geeko>
2021-10-25 16:31:06 -0700

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
-<script setup>
-import TheMainHeader from "./components/TheMainHeader.vue";
-import TheManualViewer from "./components/TheManualViewer.vue";
-</script>
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      headerRightText: "Current Page: 1 of 94",
-      headerLeftText: "",
-    };
-  },
-  methods: {
-    currentPageUpdated(data) {
-      this.headerRightText = `Current Page: ${data} of 94`;
-    },
-  },
-};
-</script>
-
 <template>
-  <div>
-    <TheMainHeader :header-right-text="headerRightText"></TheMainHeader>
-    <TheManualViewer @currentPageUpdated="currentPageUpdated"></TheManualViewer>
-  </div>
+  <router-view></router-view>
 </template>
+
+<script setup>
+</script>
 
 <style>
 </style>
